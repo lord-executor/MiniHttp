d4986d52e8fa191536e849051cda3ef802a564a6
lord-executor <executor.dev@gmail.com>
2013-01-17 14:03:29 +0100

    - implemented simple server error processor

---
+++ src/MiniHttp/RequestProcessors/ServerErrorProcessor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using MiniHttp.Server;
+using System.IO;
+
+namespace MiniHttp.RequestProcessors
+{
+    public class ServerErrorProcessor : IRequestProcessor
+    {
+        #region IRequestProcessor Members
+
+        public void ProcessRequest(RequestContext context)
+        {
+            if (context.Errors.Count > 0)
+            {
+                context.Response.ContentType = "text/plain";
+                context.Response.StatusCode = 500;
+
+                var writer = new StreamWriter(context.Response.OutputStream);
+                foreach (var e in context.Errors)
+                {
+                    writer.WriteLine("Unhandled Exception:");
+                    writer.WriteLine(e);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
