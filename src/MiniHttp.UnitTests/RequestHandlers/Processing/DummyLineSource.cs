f4802fd42bd11785ad9af81d1b18a4f6b1430ea2
lord-executor <executor.dev@gmail.com>
2013-01-24 12:54:46 +0100

    - started implementation of line iterator unit tests

---
+++ src/MiniHttp.UnitTests/RequestHandlers/Processing/DummyLineSource.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MiniHttp.RequestHandlers.Processing
+{
+    public class DummyLineSource : LineSource
+    {
+        private readonly List<Line> _lines;
+
+        public DummyLineSource(ISourceResolver resolver)
+            : base(resolver)
+        {
+            _lines = new List<Line>();
+        }
+
+        public DummyLineSource(ISourceResolver resolver, IEnumerable<string> lines)
+            : this(resolver)
+        {
+            _lines.AddRange(lines.Select(l => new Line(this, l)));
+        }
+
+        public override IEnumerator<Line> GetEnumerator()
+        {
+            return _lines.GetEnumerator();
+        }
+
+        public void Add(string line)
+        {
+            _lines.Add(new Line(this, line));
+        }
+    }
+}
---
+++ src/MiniHttp.UnitTests/RequestHandlers/Processing/LineIteratorFixture.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace MiniHttp.RequestHandlers.Processing
+{
+    [TestFixture]
+    public class LineIteratorFixture
+    {
+        private readonly string[] _lines = new[] { "a", "b", "c", "d" };
+
+        [Test]
+        public void TestInitialState()
+        {
+            var source = new DummyLineSource(null, _lines);
+            var iterator = source.GetLineIterator();
+            Assert.IsNull(iterator.Current);
+        }
+
+        [Test]
+        public void TestUnresettable()
+        {
+            var iterator = new DummyLineSource(null).GetLineIterator();
+
+            Assert.Throws<NotImplementedException>(iterator.Reset);
+        }
+
+        [Test]
+        public void TestSingleSourceIteration()
+        {
+            var source = new DummyLineSource(null, _lines);
+            var linearLines = Linearize(source.GetLineIterator());
+
+            Assert.AreEqual(_lines.Length, linearLines.Count);
+            Assert.True(_lines.Zip(linearLines, (l, line) => ReferenceEquals(l, line.Value)).All(v => v));
+        }
+
+        private IList<Line> Linearize(IEnumerator<Line> enumerator)
+        {
+            var list = new List<Line>();
+            while (enumerator.MoveNext())
+            {
+                list.Add(enumerator.Current);
+            }
+            return list;
+        }
+    }
+}
