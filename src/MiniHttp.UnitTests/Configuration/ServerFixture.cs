27680fa442cc1d4eb0cace349d9c3bf1bc8dcd11
Lukas Angerer <lord.of.war@gmx.ch>
2013-05-21 22:57:24 +0200

    - server XML configuration root element

---
+++ src/MiniHttp.UnitTests/Configuration/ServerFixture.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace MiniHttp.Configuration
+{
+    [TestFixture]
+    public class ServerFixture
+    {
+        [Test]
+        public void TestLoadConfig()
+        {
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(typeof(ServerFixture), "sample.xml"))
+            {
+                var config = Server.LoadConfig(new StreamReader(stream));
+                Assert.AreEqual(42, config.Port);
+            }
+        }
+    }
+}
---
+++ src/MiniHttp/Configuration/Server.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace MiniHttp.Configuration
+{
+    [XmlRoot("server")]
+    public class Server
+    {
+        [XmlAttribute("port")]
+        public int Port { get; set; }
+
+        public static Server LoadConfig(TextReader reader)
+        {
+            var serializer = new XmlSerializer(typeof(Server));
+            return (Server)serializer.Deserialize(reader);
+        }
+    }
+}
